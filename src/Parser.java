38de8499aae08ee8ba22fd360a03dcfecb105df9
Cory Feitelson <cfeitelson@roovy.com>
2015-02-27 20:50:46 -0800

    formatted code
    Signed-off-by: Cory Feitelson <cfeitelson@roovy.com>

--- src/Parser.java
+++ src/Parser.java
@@ -15,44 +15,39 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Parser {
-public static void main()
-{
-	try{
 	
-	
-	
-	Connection con = null;
-    Statement st = null;
-    ResultSet rs = null;
+	public static void main() {
+		try {
+			Connection con = null;
+			Statement st = null;
+			ResultSet rs = null;
 
-    String url = "jdbc:mysql://localhost:3306/dbname";
-    String user = "";
-    String password = "";
-    
-    Class.forName("com.mysql.jdbc.Driver");
-    con = DriverManager.getConnection(url, user, password);
-    st = con.createStatement();
-    rs = st.executeQuery("SELECT * FROM healthmessagesexchange.messagequeue ORDER BY control_id DESC LIMIT 1;");
-    
-    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-	
-    DocumentBuilder db = dbf.newDocumentBuilder();
-	
-	Document dom = db.parse(rs.getString(0));
-	
-	}catch(ParserConfigurationException pce) {
-		pce.printStackTrace();
-	}catch(SAXException se) {
-		se.printStackTrace();
-	}catch(IOException ioe) {
-		ioe.printStackTrace();
-	} catch (ClassNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+			String url = "jdbc:mysql://localhost:3306/dbname";
+			String user = "";
+			String password = "";
+
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(url, user, password);
+			st = con.createStatement();
+			rs = st.executeQuery("SELECT * FROM healthmessagesexchange.messagequeue ORDER BY control_id DESC LIMIT 1;");
+
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+			DocumentBuilder db = dbf.newDocumentBuilder();
+
+			Document dom = db.parse(rs.getString(0));
+
+		}catch(ParserConfigurationException pce) {
+			pce.printStackTrace();
+		}catch(SAXException se) {
+			se.printStackTrace();
+		}catch(IOException ioe) {
+			ioe.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-}
 }
 
